39fc21a0c6c0673c58b364ce574aaceb4d1d210a
Itay Radotzki <radotzki@gmail.com>
2016-01-05 09:41:37 +0200

    gmm clustering

--- clustering.py
+++ clustering.py
@@ -1,6 +1,7 @@
 from sklearn.cluster import KMeans
-# from sklearn.mixture import GMM
+from sklearn.mixture import GMM
 import numpy
+import numpy as np
 import pandas as pd
 
 
@@ -45,34 +46,81 @@ def evaluate_coalition(df, coalition):
     print 'Coalition: ' + str(coalition) + ", size: " + str(len(coalition_df)) + ", score: " + str(score)
 
 
-def main():
-    df = pd.read_csv('./dataset/transformed_train.csv')
+def binary_transformation(df):
     df['Will_vote_only_large_party'] = df['Will_vote_only_large_party'].map(lambda x: 1 if x==1 else -1)
     df['Financial_agenda_matters'] = df['Financial_agenda_matters'].map(lambda x: 1 if x==1 else -1)
 
-    n_clusters = 100
-    cls = KMeans(n_clusters=n_clusters)
-    res = cls.fit_predict(df.drop('Vote', axis=1))
 
-    for v in xrange(n_clusters):
-        sum_ = sum(get_votes_count_by_party(df.iloc[[x for x, y in enumerate(res) if y==v]]).itervalues())
-        print max({k: (v + .0) / sum_ for k, v in get_votes_count_by_party(df.iloc[[x for x,y in enumerate(res) if y==v]]).iteritems()}.itervalues())
-
-    coalition = set()
-    minimal_coalitions = []
-    get_minimal_coalitions(coalition, 0, minimal_coalitions)
-
-
-    for coalition in minimal_coalitions:
-        evaluate_coalition(df.drop('Most_Important_Issue', axis=1), coalition)
-
-
-    values = [int(x) for x in df['Most_Important_Issue'].unique()]
-    values.sort()
-    for i in values:
-        df['Most_Important_Issue_'+str(i)] = df['Most_Important_Issue'].map(lambda x: 1 if x==i else 0)
+def dummis_transformation(df):
+    dummies_df = pd.get_dummies(df['Most_Important_Issue'], prefix='Most_Important_Issue')
+    df = pd.concat([df, dummies_df], axis=1, join='inner')
     df.drop('Most_Important_Issue', axis=1, inplace=True)
-    df.describe()
+
+
+def main():
+    df = pd.read_csv('./dataset/transformed_train.csv')
+    binary_transformation(df)
+
+    # n_clusters = 100
+    # cls = KMeans(n_clusters=n_clusters)
+    # res = cls.fit_predict(df.drop('Vote', axis=1))
+    #
+    # for v in xrange(n_clusters):
+    #     sum_ = sum(get_votes_count_by_party(df.iloc[[x for x, y in enumerate(res) if y==v]]).itervalues())
+    #     print max({k: (v + .0) / sum_ for k, v in get_votes_count_by_party(df.iloc[[x for x,y in enumerate(res) if y==v]]).iteritems()}.itervalues())
+    #
+    # coalition = set()
+    # minimal_coalitions = []
+    # get_minimal_coalitions(coalition, 0, minimal_coalitions)
+    #
+    #
+    # for coalition in minimal_coalitions:
+    #     evaluate_coalition(df.drop('Most_Important_Issue', axis=1), coalition)
+
+    dummis_transformation(df)
+
+    X_train = df.drop('Vote', axis=1)
+    y_train = df.Vote.values
+
+    lowest_aic = np.infty
+    aic = []
+    # TODO: keep only the commented out lines
+    # n_components_range = range(1, 20)
+    # cv_types = ['spherical', 'tied', 'diag', 'full']
+    n_components_range = [18]
+    cv_types = ['diag']
+
+    for cv_type in cv_types:
+        for n_components in n_components_range:
+            gmm = GMM(n_components=n_components, covariance_type=cv_type)
+
+            # Since we have class labels for the training data, we can
+            # initialize the GMM parameters in a supervised manner.
+            gmm.means_ = np.array([X_train[y_train == i].mean(axis=0) for i in xrange(n_components)])
+
+            # Fit a mixture of Gaussians with EM
+            gmm.fit(X_train)
+
+            aic.append(gmm.aic(X_train))
+            print 'n_components: ' + str(n_components) +\
+                    ' cv_type: ' + str(cv_type) + \
+                    ' aic: ' + str(gmm.aic(X_train)) + \
+                    ' bic: ' + str(gmm.bic(X_train))
+            if aic[-1] < lowest_aic:
+                lowest_aic = aic[-1]
+                best_gmm = gmm
+
+    print 'best model:'
+    print '\tn_components = ' + str(best_gmm.n_components)
+    print '\tcovariance_type = ' + str(best_gmm.covariance_type)
+    print '\taic = ' + str(lowest_aic)
+
+    y_train_pred = best_gmm.predict(X_train)
+    for v in xrange(best_gmm.n_components):
+        cluster = df.iloc[[x for x, y in enumerate(y_train_pred) if y==v]]
+        print get_votes_count_by_party(cluster)
+
+
 
 if __name__ == "__main__":
     main()
